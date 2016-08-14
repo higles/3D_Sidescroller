b553810dd4a6769935b9f9a581b61b060b626ac4
chaseguyer <chaseguyer@gmail.com>
2016-08-14 14:51:00 -0700

    Changed Menu to paper theme

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	private static MusicManager instance = null;
+
+	public static MusicManager Instace { get { return instance; } }
+
+	void Awake()
+	{
+		if(instance == null)
+		{
+			Destroy(instance);
+		}
+		else if(instance == this)
+		{
+			DontDestroyOnLoad(instance);
+		}
+
+	}
+}
