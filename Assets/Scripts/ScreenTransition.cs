550d9fb9eb1ee4598de550a2c9fd336c836e1d43
chaseguyer <chaseguyer@gmail.com>
2016-08-14 15:30:41 -0700

    Created Settings Menu and Handled Transition

---
+++ Assets/Scripts/ScreenTransition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenTransition : MonoBehaviour {
+
+	GameObject mainMenu;
+	GameObject settingsMenu;
+
+	// Use this for initialization
+	void Start () 
+	{
+		mainMenu = GameObject.FindGameObjectWithTag("Main Menu Tag");
+		settingsMenu = GameObject.FindGameObjectWithTag("Settings Menu Tag");
+
+		mainMenu.SetActive(true);
+		settingsMenu.SetActive(false);
+	}
+	
+	public void transitionToSettings ()
+	{
+		mainMenu.SetActive(false);
+		settingsMenu.SetActive(true);
+
+	}
+
+	public void transitionToMainMenu ()
+	{
+		mainMenu.SetActive(true);
+		settingsMenu.SetActive(false);
+	}
+}
